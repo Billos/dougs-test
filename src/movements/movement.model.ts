6f3292604bb20d0cb32303662c75bacc35e008f5
billos <billos@billos.fr>
2025-05-07 15:14:12 +0200

    feat(movements): define models

---
+++ src/movements/movement.model.ts
@@ -0,0 +1,33 @@
+export type Movement = {
+  id: number;
+  date: Date;
+  label: string;
+  amount: number;
+};
+
+export type Balance = {
+  date: Date;
+  balance: number;
+};
+
+export enum ValidationErrorMessage {
+  BalancesMissing = 'At least one balance is missing',
+  WithdrawalDifference = 'At least one withdrawal is missing',
+  DepositDifference = 'At least one deposit is missing',
+  Duplicate = 'A duplicate is found',
+}
+
+/**
+ * ValidationError represents an error that occurs during the validation of movements and balances.
+ * Can be one of the following:
+ * - Duplicate: Indicates that a duplicate is found.
+ * - Difference: Indicates at least one movement is missing.
+ * details: Optional details about the error. Such as the duplicate id for instance.
+ */
+export type ValidationError = {
+  type: 'Duplicate' | 'Difference' | 'BalancesMissing';
+  message: ValidationErrorMessage;
+  details?: string;
+  period?: { start: Date; end: Date };
+  difference?: number;
+};
